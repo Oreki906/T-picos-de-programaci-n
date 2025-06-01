eefdeb728ebb4d27e126936306e55052e5d866e3
flore <flore@LAPTOP-92DN0AH1>
2025-06-01 10:50:15 -0700

    agraregar

---
+++ Interfas grafica dinamica/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Interfas_grafica_dinamica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRuta = new System.Windows.Forms.TextBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.userControl11 = new Estrada.userControl1();
+            this.SuspendLayout();
+            // 
+            // txtRuta
+            // 
+            this.txtRuta.Location = new System.Drawing.Point(12, 11);
+            this.txtRuta.Name = "txtRuta";
+            this.txtRuta.Size = new System.Drawing.Size(241, 22);
+            this.txtRuta.TabIndex = 0;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(216, 39);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 1;
+            this.btnIniciar.Text = "Buscar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // userControl11
+            // 
+            this.userControl11.Location = new System.Drawing.Point(201, 241);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(123, 62);
+            this.userControl11.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 450);
+            this.Controls.Add(this.userControl11);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.txtRuta);
+            this.Name = "Form1";
+            this.Text = "form1 ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRuta;
+        private System.Windows.Forms.Button btnIniciar;
+        private Estrada.userControl1 userControl11;
+    }
+}
+
---
+++ Interfas grafica dinamica/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Interfas_grafica_dinamica
+{
+    public partial class Form1 : Form
+    {
+        private FlowLayoutPanel flowLayoutPanel;
+        private TextBox textBox;
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtRuta.Text))//asegura que alla algo en el texvox
+            {
+                string ruta=txtRuta.Text;//extrae la ruta
+                flowLayoutPanel = new FlowLayoutPanel();//crea el panel donde apareceran las imagenes
+                //flowlayautopanel es un control que organiza los controles de flujo orita seran las imagenes
+                flowLayoutPanel.Dock = DockStyle.Fill;//dok para que utilise un espacio especifico y fill para que sea todo el espacio
+                this.Controls.Add(flowLayoutPanel);// hay que darle el control del formulario
+                SacarImagenes(ruta);//llamo al metodo de sacar las imagenes
+
+                //hacer invisibles los de inicio
+                btnIniciar.Visible = false;
+                txtRuta.Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("no se a especificado una ruta");//si no hay ruta 
+            }
+
+        }
+
+        public void SacarImagenes(string ver)// metodo para sacar las imagenes
+        {
+
+            //el directori viene del .IO debuelve las rutas de las imagenes (no solo sirve para imagenes)
+            //el var es para no especificar un tipo de variable
+            //el forec es para revisar cada imagen
+            foreach(var verimagen in Directory.GetFiles(ver,"*png",SearchOption.AllDirectories))//el profe ayudo con eso 
+            {
+
+                var nombre = Path.GetFileName(verimagen);
+                //crear los espacios para las imagenes, opcion 2 si la prueba de los botones sale mal
+
+
+                var pictureBox = new PictureBox
+                {
+                    //son las propiedades de la imagen que se van a mostrar, tamaño al azar de prueba y error
+                    Image = Image.FromFile(verimagen),
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+                    Size = new Size(120,120),
+                    
+
+                };
+                //evento al hacer clic en una imagen sigue el hacer grande la imagen
+                pictureBox.Click += (sender, e) => verimagengrande(verimagen, nombre);
+                flowLayoutPanel.Controls.Add(pictureBox);
+            }
+        }
+
+        //metodo para ver la imagen grande
+        //es basicamente lo mismo que el anterior peri sin usar el controlado de la organizacion 
+        //orientado a una sola imagen en lugar de n  
+        public void verimagengrande(string ver, string nombre)
+        {
+            //generar el formulario donde se vera la imagen grande
+            var imagengrande = new Form
+            //medidas y color del form 
+            {
+                Width = 800,
+                Height = 600,
+                BackColor= Color.Black,
+                Text = nombre
+
+            };
+            //generar el espacio para la imagen seleccionada 
+            var pictureBox = new PictureBox
+            {
+                Image = Image.FromFile(ver),
+                Dock = DockStyle.Fill,
+                SizeMode = PictureBoxSizeMode.StretchImage,
+            };
+            //agreagr y mostar la imagen 
+            imagengrande.Controls.Add(pictureBox);
+            imagengrande.Show();
+           
+        }
+        //pendiente: que se muestre el nombre de la uimagen en el titulo de la pestaña grande 
+
+    }
+}
